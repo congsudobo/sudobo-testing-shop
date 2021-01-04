92b96d93a76f3845305638ab12842e0ed6c623d4
Phạm Văn Công <phamvancong@MacBook-Pro-cua-Pham.local>
2021-01-04 18:27:15 +0700

    Add Show List Products

--- backend/app/Services/ProductService.php
+++ backend/app/Services/ProductService.php
@@ -52,4 +52,70 @@ Class ProductService
 
         return $product;
     }
+
+    public function destroy($id) {
+        $storeImagePath = config("product.product_image_path");
+        $product = Products::where('id',$id)
+        ->withTrashed()
+        ->first();
+
+        if(!$product) {
+            return false;
+        }
+
+        try {
+            \DB::beginTransaction();
+
+            $product->delete();
+
+            \DB::commit();
+            $this->deleteFile("$storeImagePath\\$product->product_image");
+        } catch(\PDOException $ex) {
+            \DB::rollback();
+            return false;
+        }
+
+        return true;
+    }
+
+    public function index($request) {
+        $productQuery = Products::with("category");
+
+        $this->buildSortQuery($request, $productQuery);
+        $this->buildPagination($request, $productQuery);
+
+        $products = $productQuery->get();
+        return $products;
+    }
+
+    private function buildPagination($request, $query) {
+        $pagination = $request->input("limit") ?? config("product.pagination.max_item");
+
+        $query->paginate($pagination);
+    }
+
+    private function buildSortQuery($request, $query) {
+        $searchId = $request->input("searchId");
+        $addedStart = $request->input("addedStart");
+        $addedEnd = $request->input("addedEnd");
+        $expirationStart = $request->input("expirationStart");
+        $expirationEnd = $request->input("expirationEnd");
+        $categoryId = $request->input("categoryId");
+
+        if($searchId) {
+            $query->where("id", $searchId);
+        }
+
+        if($addedStart && $addedEnd) {
+            $query->whereBetween("added_date", [$addedStart, $addedEnd]);
+        }
+
+        if($expirationStart && $expirationEnd) {
+            $query->whereBetween("expiration_date", [$expirationStart, $expirationEnd]);
+        }
+
+        if($categoryId) {
+            $query->where("category_id", $categoryId);
+        }
+    }
 }
