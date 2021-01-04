fb73ac2f92270ab26342aae8dcaf7e2cb428c9d8
Phạm Văn Công <phamvancong@MacBook-Pro-cua-Pham.local>
2021-01-05 01:52:30 +0700

    fetures/create-home-page

---
+++ backend/resources/views/common/header.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+    <!-- Sidebar Toggle (Topbar) -->
+    <a href="/">Trang chủ</a>
+
+    <!-- Topbar Search -->
+</nav>
