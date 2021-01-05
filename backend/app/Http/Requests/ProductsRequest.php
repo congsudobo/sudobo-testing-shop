008c85b8c891f65b217ddc81b25eabcffea2e82e
Phạm Văn Công <phamvancong@MacBook-Pro-cua-Pham.local>
2021-01-05 15:49:18 +0700

    Fix Form Request Code

--- backend/app/Http/Requests/ProductsRequest.php
+++ backend/app/Http/Requests/ProductsRequest.php
@@ -24,11 +24,13 @@ class ProductsRequest extends FormRequest
     public function rules()
     {
         return [
+            'id' => 'integer',
             'product_name' => 'required|string',
             'added_date' => 'required|date',
             'count' => 'integer:value',
             'expiration_date' => 'required|date',
             "product_image" => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            "category_id" => 'required|integer',
         ];
     }
 }
